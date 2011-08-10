8fadcd806682179e877bd8ced7c3bcf982a8657d
Evgeni Sokolov <e.sokolov@itransition.com>
2011-08-10 10:31:02 +0300

    Product db fixes

--- Source/Products.Migrations/Migration_AddCategoryLocales.cs
+++ Source/Products.Migrations/Migration_AddCategoryLocales.cs
@@ -14,13 +14,13 @@ namespace Products.Migrations
         /// </summary>
         public override void Up()
         {
-            Database.AddTable("News_CategoryLocales", t =>
+            Database.AddTable("Product_CategoryLocales", t =>
             {
                 t.PrimaryKey();
                 t.String("Title");
                 t.Text("Description");
                 t.Text("Culture").Length(10);
-                t.ForeignKey("NewsCategoryId").Table("News_Categories").OnDelete(ForeignKeyConstraint.Cascade);
+                t.ForeignKey("CategoryId").Table("Product_Categories").OnDelete(ForeignKeyConstraint.Cascade);
 
             });
         }
@@ -30,8 +30,8 @@ namespace Products.Migrations
         /// </summary>
         public override void Down()
         {
-            Database.ChangeTable("News_CategoryLocales", t => t.RemoveForeignKey("NewsCategoryId").Table("News_Categories"));
-            Database.RemoveTable("News_CategoryLocales");
+            Database.ChangeTable("Product_CategoryLocales", t => t.RemoveForeignKey("ProductCategoryId").Table("Product_Categories"));
+            Database.RemoveTable("Product_CategoryLocales");
         }
     }
    
